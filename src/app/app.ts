8ae86e5c405d4d1fa42be19b875919315cfda24d
itsarpit05 <imarpitsachan05@gmail.com>
2026-09-01 10:50:17 +0530

    Component creation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true
 })
 export class App {
   // protected readonly title = signal('Angular-Learning');
